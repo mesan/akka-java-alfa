ebf4dd1f711f5c47639ba4a617e6fa0b2d4e4351
Øystein Skadsem <oysteins@mesan.no>
2015-07-07 16:52:37 +0200

    Sørget for at rot-actor sender videre til under-actors url'en som skal scannes

--- src/main/java/no/mesan/akka/WikipediaParserMaster.java
+++ src/main/java/no/mesan/akka/WikipediaParserMaster.java
@@ -1,7 +1,13 @@
 package no.mesan.akka;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
 import akka.japi.pf.ReceiveBuilder;
+import no.mesan.akka.actors.ImageFinder;
+import no.mesan.akka.linkFinder.LinkFinder;
+import no.mesan.akka.summary.ParsePage;
+import no.mesan.akka.summary.SummaryActor;
 
 public class WikipediaParserMaster extends AbstractActor {
 
@@ -14,5 +20,9 @@ public class WikipediaParserMaster extends AbstractActor {
 
     private void handleScanRequest(final WikipediaScanRequest wikipediaScanRequest) {
         System.out.println("Starter scan av url: " + wikipediaScanRequest.getUrl());
+        final ActorRef self = context().self();
+        context().actorOf(Props.create(ImageFinder.class)).tell(wikipediaScanRequest, self);
+        context().actorOf(Props.create(LinkFinder.class)).tell(wikipediaScanRequest, self);
+        context().actorOf(Props.create(SummaryActor.class)).tell(new ParsePage(wikipediaScanRequest.getUrl()), self);
     }
 }
